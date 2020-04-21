9b9126482e3868b395b37127b57b2b290243aa6d
jhon11390 <meljhon2@gmail.com>
2020-04-21 03:52:14 -0500

    Se realiza captura de input y se crean condicionales

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
-    let numero = Math.round(Math.random()*10000)
-    console.log(numero)
+    let magicnumber = Math.floor(Math.random()*(9999-1000))+1000;
+    console.log(magicnumber);
+    let magicnumberstring = magicnumber.toString();
+    let arraymagicnumber = magicnumberstring.split('');
+    console.log(arraymagicnumber.some(arr=> arr=="2"))
+    $('#numero').on('keyup', function(Tecla){
+        arraymagicnumber;
+        let valor = $(this).val();
+        let valorstring = valor.toString();
+        let arrayvalor = valorstring.split('');
+        let picas = [];
+        let fijas = [];
+        arrayvalor.forEach(element => {
+            if(arraymagicnumber.some(arr=> arr==element)){
+                if(arrayvalor.indexOf(element) == arraymagicnumber.indexOf(element)){
+                    fijas.push(element)
+                }else {
+                    picas.push(element)
+                }
+                
+            };
+        });
+
+
+        if(Tecla.keyCode == 13){
+            $('tbody').append('<tr><td>'+ valor + '</td><td>'+ picas.length + '</td><td>'+ fijas.length + '</td></tr>');
+            $(this).val('');
+        };
+    });
 });
